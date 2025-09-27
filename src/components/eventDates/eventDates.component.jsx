6acd2d136ae880ccf6e6ca8b0f28e971fc71af5c
Eric Onuoha <plipsd@gmail.com>
2025-09-27 01:07:48 +0100

    Update trad date

--- src/components/eventDates/eventDates.component.jsx
+++ src/components/eventDates/eventDates.component.jsx
@@ -2,7 +2,7 @@ import EventDatesPreview from "./eventDatesPreview/eventDatesPreview.component"
 
 const EventDates = () => {
     const dates = [
-        {name:"Traditional Wedding", date:"Thursday December 9th, 2025 - Minna, Niger State", details: "Come and be a part of this unique celebration of the marrying of multiple cultures as we celebrate our wedding and acknowledge our heritage and families"},
+        {name:"Traditional Wedding", date:"Tuesday December 9th, 2025 - Minna, Niger State", details: "Come and be a part of this unique celebration of the marrying of multiple cultures as we celebrate our wedding and acknowledge our heritage and families"},
         {name:"Church Solemnization", date:"Saturday December 13th, 2025 - Abuja", details:"Join us in our wedding solemnization which will hold at The Lighthouse Church located at Utako, Abuja from 9:00am" }
     ]
     return(
